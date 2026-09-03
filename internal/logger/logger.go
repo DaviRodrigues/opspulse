3f9cce9a7fafb939149e5637bb8355daf4a39ca9
DaviRodrigues <oversouls11@gmail.com>
2026-09-03 13:42:49 -0300

    feat: implement logging setup and add tests for logger functionality

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,5 +1,50 @@
 package logger
 
-func Setup(level, format, logFilePath string) {
-	
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func makePathLog(logDir string) (io.Writer, error) {
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return nil, err
+	}
+
+	fileName := fmt.Sprintf("opspulse-%s.log", time.Now().Format("2006-01-02"))
+	fullPath := filepath.Join(logDir, fileName)
+
+	file, err := os.OpenFile(fullPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	return io.MultiWriter(os.Stdout, file), nil
+}
+
+func SetupSlog(handler slog.Handler) (*slog.Logger, error) {
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+	return logger, nil
+}
+
+func HandlerDefaultJSON(level slog.Level, logDir string) (slog.Handler, error) {
+	multiWriter, err := makePathLog(logDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{Level: level}), nil
+}
+
+func HandlerDefaultText(level slog.Level, logDir string) (slog.Handler, error) {
+	multiWriter, err := makePathLog(logDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return slog.NewTextHandler(multiWriter, &slog.HandlerOptions{Level: level}), nil
 }
---
+++ internal/logger/logger_test.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestSetupLogger(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	handler, err := HandlerDefaultText(slog.LevelInfo, tmpDir)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	logger, err := SetupSlog(
+		handler,
+	)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	logger.Info("teste de log")
+
+	expectedFile := filepath.Join(tmpDir, fmt.Sprintf("opspulse-%s.log", time.Now().Format("2006-01-02")))
+	contentBytes, err := os.ReadFile(expectedFile)
+	if err != nil {
+		t.Fatalf("erro ao ler arquivo de log: %v", err)
+	}
+
+	content := string(contentBytes)
+	if !strings.Contains(content, "teste de log") {
+		t.Errorf("esperava encontrar 'teste de log' no arquivo, mas o conteúdo foi: %s", content)
+	}
+}
