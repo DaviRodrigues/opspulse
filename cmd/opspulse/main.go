6a929b6d35afb6a02baa3f201e285764a9698a1a
DaviRodrigues <oversouls11@gmail.com>
2026-09-03 14:21:10 -0300

    refactor: improve logging setup and error handling in main function

--- cmd/opspulse/main.go
+++ cmd/opspulse/main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/DaviRodrigues/opspulse/internal/checker"
 	"github.com/DaviRodrigues/opspulse/internal/config"
 	"github.com/DaviRodrigues/opspulse/internal/context"
-//	"github.com/DaviRodrigues/opspulse/internal/discord"
+	"github.com/DaviRodrigues/opspulse/internal/logger"
+	// "github.com/DaviRodrigues/opspulse/internal/discord"
 )
 
 func main() {
+	handler, err := logger.HandlerDefaultText(slog.LevelInfo, "./log")
+	if err != nil {
+		slog.Error("Não foi possível carregar o handler do log", "error", err)
+		os.Exit(1)
+	}
+
+	err = logger.SetupSlog(handler)
+	if err != nil {
+		slog.Error("Não foi possível iniciar o log", "error", err)
+		os.Exit(1)
+	}
+
 	ctx, stop := context.CreateNotifyContext()
 	defer stop()
 
-	cfg, err := config.Load()
+	cfg, err := config.Load("./.env.test")
 	if err != nil {
-		fmt.Printf("%v", err)
+		slog.Error("Falha crítica ao carregar configurações", "error", err)
+		os.Exit(1)
 	}
 
 //	bot, err := discord.New(cfg.DiscordToken, cfg.DiscordChannelID)
 //	if err != nil {
-//		fmt.Printf("%v", err)
+//		slog.Warn("Falha ao iniciar bot", "error", err)
 //	}
 //	defer bot.Close()
 
 	checker.StartMonitoring(ctx, nil, cfg.TargetURLs, cfg.CheckInterval, cfg.CheckTimeout)
-
 }
