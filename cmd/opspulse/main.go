29cb12187e89ce27e6633405c26158497a4832f8
DaviRodrigues <oversouls11@gmail.com>
2026-09-04 15:50:46 -0300

    fix: refactor bot initialization and configuration loading in main function

--- cmd/opspulse/main.go
+++ cmd/opspulse/main.go
@@ -7,8 +7,8 @@ import (
 	"github.com/DaviRodrigues/opspulse/internal/checker"
 	"github.com/DaviRodrigues/opspulse/internal/config"
 	"github.com/DaviRodrigues/opspulse/internal/context"
-	"github.com/DaviRodrigues/opspulse/internal/logger"
 	"github.com/DaviRodrigues/opspulse/internal/discord"
+	"github.com/DaviRodrigues/opspulse/internal/logger"
 )
 
 // TODO preciso depois testar a integração disso de forma manual (remova o .env.test NÃO ESQUECER)
@@ -29,29 +29,29 @@ func main() {
 	ctx, stop := context.CreateNotifyContext()
 	defer stop()
 
-	cfg, err := config.Load("./.env.test")
+	cfg, err := config.Load()
 	if err != nil {
 		slog.Error("Falha crítica ao carregar configurações", "error", err)
 		os.Exit(1)
 	}
 
-	bot, err := discord.New(cfg.Discord.Token, cfg.Discord.ChannelID)
-	if err != nil {
-		slog.Warn("Falha ao iniciar bot", "error", err)
-	}
-	defer bot.Close()
-
+	var bot *discord.Bot
 	triggerChan := make(chan struct{}, 1)
-	bot.RegisterHandlers(func() []checker.CheckResult {
-		results := checker.CheckAll(ctx, cfg.Monitor.TargetURLs, cfg.Monitor.Timeout)
-		
-		select {
-		case triggerChan <-struct{}{}:
-		default:
-		}
+	bot, err = discord.New(cfg.Discord.Token, cfg.Discord.ChannelID)
+	if err != nil {
+		slog.Warn("Não foi possível iniciar o bot do Discord, continuando apenas com monitor local", "error", err)
+	} else {
+		defer bot.Close()
+		bot.RegisterHandlers(func() []checker.CheckResult {
+			results := checker.CheckAll(ctx, cfg.Monitor.TargetURLs, cfg.Monitor.Timeout)
 
-		return results
-	})
+			select {
+			case triggerChan <- struct{}{}:
+			default:
+			}
+			return results
+		})
+	}
 
 	checker.StartMonitoring(ctx, bot, triggerChan, cfg)
 }
